4f89faa38fbd4b8ad6829ac888e38e3db703f462
Raphael <raphael.pb.rialland@gmail.com>
2025-11-20 23:14:57 +0100

    placeholder

---
+++ nano_batch_rs/src/lib.rs
@@ -0,0 +1,15 @@
+mod block_allocator;
+mod request;
+mod scheduler;
+
+pub fn temp() {
+    println!("Hello, world!"); 
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("Hello, world!");
+    }
+}
